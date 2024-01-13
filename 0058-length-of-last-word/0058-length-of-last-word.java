10af17099607daad2021186e6259fcf4d3d02a5f
Buse Keklik <76210283+busekeklik@users.noreply.github.com>
2024-01-13 14:14:05 +0300

    Time: 0 ms (100.00%), Space: 41.9 MB (11.37%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int count = 0;  
+        int n = s.length();
+        for(int i = n -1; i >= 0; i--)
+        {
+            if(s.charAt(i) != ' ')
+            {
+                count++;
+            }
+            else
+            {
+                if(count > 0)
+                {
+                    return count;
+                }
+            }    
+        }
+    return count;
+    }
+}
