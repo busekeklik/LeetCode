783f3546644ac0c0aa17716b4edacf3cc4b765b6
Buse Keklik <76210283+busekeklik@users.noreply.github.com>
2024-01-18 20:18:17 +0300

    Time: 2 ms (84.61%), Space: 46.3 MB (7.24%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+       
+        int max = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+             if (i > max) {
+                return false;
+            }
+            max = Math.max(max, i + nums[i]);
+            if (max >= nums.length - 1) {
+                return true;
+            }
+        }
+       
+        return false;
+    }
+}
