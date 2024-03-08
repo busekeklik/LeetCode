cc502d710d790a9d41709e7c0ebd0ec98531e2a6
Buse Keklik <76210283+busekeklik@users.noreply.github.com>
2024-03-08 10:58:57 +0300

    Time: 0 ms (100.00%), Space: 40.2 MB (96.97%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if(n<=0)
+            return false;
+        while(n%4==0){
+            n/=4;
+        }
+        if(n==1)
+            return true;
+        else
+            return false;
+    }
+}
