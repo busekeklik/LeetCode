1520f57920d374ac5c2b7f0f6d709163b94bf4fe
Buse Keklik <76210283+busekeklik@users.noreply.github.com>
2024-03-14 20:53:25 +0300

    Time: 8 ms (91.14%), Space: 44.4 MB (13.86%) - LeetHub

---
+++ 0326-power-of-three/0326-power-of-three.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        if(n<1){
+            return false;
+        }
+        while(n>1){
+            if(n%3!=0){
+                return false;
+            }
+            n = n/3;
+        }
+        return true;
+    }
+}
+
+
