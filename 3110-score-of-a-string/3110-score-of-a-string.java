1a2abedd642f135bf8d06c0dedcd88012482a18a
Buse Keklik <76210283+busekeklik@users.noreply.github.com>
2024-04-15 23:54:26 +0300

    Time: 1 ms (99.97%), Space: 41.6 MB (93.94%) - LeetHub

---
+++ 3110-score-of-a-string/3110-score-of-a-string.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int scoreOfString(String s) {
+        int sum=0;
+        for(int i=1;i<s.length();i++)
+            sum+=Math.abs((int)s.charAt(i)-(int)s.charAt(i-1));
+        return sum;
+    }
+}
